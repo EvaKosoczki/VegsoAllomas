8da0d023912b607c4ae46afb405db6783f957006
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-09-23 21:35:08 +0200

    http get with parameters

--- webshop-angular/src/app/service/products.service.ts
+++ webshop-angular/src/app/service/products.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 import { Product } from '../model/product';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
-  routeUrl:string ='http://localhost:3000/admin/products'
-  constructor(private http: HttpClient) { }
+  routeUrl: string = '';
+
+  constructor(private http: HttpClient,
+    private router: Router) {
+    if (this.router.url == '/products') {
+      this.routeUrl = 'http://localhost:3000/admin/products'
+    }
+  }
 
   getAll(): Observable<Product[]> {
     return this.http.get<Product[]>(this.routeUrl)

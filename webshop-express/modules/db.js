8783698313551f1d22aae02416b2cdc01022767e
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-09-23 12:11:11 +0200

    db.js readjoin for getall and getone functions

--- webshop-express/modules/db.js
+++ webshop-express/modules/db.js
@@ -1,7 +1,10 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  database: 'webshop', user: 'root', password: 'root', connectionLimit: 5,
+  database: 'webshop',
+  user: 'root',
+  password: 'root',
+  connectionLimit: 5,
 });
 
 module.exports = class DB {
@@ -21,7 +24,7 @@ module.exports = class DB {
        s.postfix,
        s.price,
        s.picture
-      FROM snowboards s JOIN brands b ON s.brand = b.id        
+      FROM snowboards as s JOIN brands as b ON s.brand = b.id        
       `;
 
     const result = await this.conn.query(sql);
@@ -72,6 +75,27 @@ module.exports = class DB {
     return result;
   } */
   //--------------------------------------------------------------------------------------------
+  async readJoin(table1, table2, column1, column2, postfix, ...args) {
+    let sql = ``;
+    if (postfix == 0) {
+      sql = `
+      SELECT ${args}
+      FROM ${table1} JOIN ${table2} ON ${table1}.${column1} = ${table2}.${column2}
+      LIMIT 12
+      `;
+    } else {
+      sql = `
+      SELECT ${args}
+      FROM ${table1} JOIN ${table2} ON ${table1}.${column1} = ${table2}.${column2}
+      WHERE postfix= '${postfix}'
+      `;
+    }
+
+
+    const result = await this.conn.query(sql);
+    return result;
+  }
+
 
   create() {}
 
--- webshop-express/routes/products.js
+++ webshop-express/routes/products.js
@@ -7,20 +7,30 @@ const DB = require('../modules/db');
 const db = new DB();
 
 /* bl-snowboards.js betöltése, majd ha megy a paraméterezős lekérdezés */
-/* const BL = require('../business-logic-layer/bl-snowboards');
+const BL = require('../business-logic-layer/bl-snowboards');
 
-const bl = new BL(); */
+const bl = new BL();
 
-
-/* GET all producst in JSON format */
-router.get('/', async (req, res, next) => {
-  const productDetails = await db.read();
+router.get('/:postfix', async (req, res, next) => {
+  const productDetails = await bl.readSnowboards(req.params.postfix);
 
   res.render('products', {
     title: 'Our products!',
     products: productDetails,
   });
 });
+
+/* GET all producst in JSON format */
+router.get('/', async (req, res, next) => {
+  const productDetails = await bl.readSnowboards();
+
+  res.render('products', {
+    title: 'Our products!',
+    products: productDetails,
+  });
+});
+
+
 
 
 router.get('/*', (req, res, next) => {

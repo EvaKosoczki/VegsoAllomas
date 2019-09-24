346446d5983735ac4186567c59999314b817d0f5
Tamás Takács <tamas.takacs91@gmail.com>
2019-09-24 12:10:57 +0200

    Feat: added basket.pug and basic routing

--- webshop-express/app.js
+++ webshop-express/app.js
@@ -9,6 +9,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
 const apiRouter = require('./routes/api');
+const basketRouter = require('./routes/basket');
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/api', apiRouter);
+app.use('/basket', basketRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {

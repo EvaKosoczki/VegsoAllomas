66a3b68d71d00f2c8cda668ec0611756e838ea36
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-09-25 16:07:01 +0200

    register, save in users, client side password validation

--- webshop-express/routes/register.js
+++ webshop-express/routes/register.js
@@ -2,10 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
+const DB = require('../modules/db');
 
+const db = new DB();
+
+const regexPattern = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{7,}$/
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('register', { title: 'Index' });
+  res.render('register', {
+    regexPattern: regexPattern
+  });
+});
+
+
+router.post('/', async (req, res, next) => {
+  const newUser = await db.create({
+    table: 'users',
+    values: req.body,
+  })
+  res.redirect('/login')
+  res.json(newUser);
 });
 
 module.exports = router;

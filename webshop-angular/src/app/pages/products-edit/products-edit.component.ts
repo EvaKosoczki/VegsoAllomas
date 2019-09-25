8c81ce9212bad682dc875b9712e1b9cae9e0ccb7
Norbert Tóth <ntoth1990@gmail.com>
2019-09-25 11:12:45 +0200

    feat 03-story:brand service, select brands foredit

--- webshop-angular/src/app/pages/products-edit/products-edit.component.ts
+++ webshop-angular/src/app/pages/products-edit/products-edit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/model/product';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from 'src/app/service/products.service';
+import { Brand } from 'src/app/model/brand';
+import { BrandsService } from 'src/app/service/brands.service';
 
 @Component({
   selector: 'app-products-edit',
@@ -11,29 +13,37 @@ import { ProductsService } from 'src/app/service/products.service';
 export class ProductsEditComponent implements OnInit {
   oneProduct: Product;
   selectedProductId: number = 0;
+  brands:Brand[];
+  selectedBrand:number;
 
   constructor(private productsService: ProductsService,
-    private ar: ActivatedRoute, private router: Router) {
+    private ar: ActivatedRoute, private router: Router, private brandsService:BrandsService) {
     this.ar.params.forEach(
       params => {
         this.selectedProductId = params.id
         this.productsService.getOne(this.selectedProductId).subscribe(
           data => {
+            this.selectedBrand=data[0].brand;
             this.oneProduct = data[0];
-        console.log(this.oneProduct);
+            console.log(this.oneProduct);
+            console.log(this.selectedBrand);
           }
-          
         )
       }
     )
   }
 
   ngOnInit() {
-
+    this.brandsService.getBrands().forEach(brands =>{
+      this.brands =brands;
+      console.log(this.brands);
+    })
   };
 
   onSubmit(ev: Event) {
     ev.preventDefault();
+    console.log(this.selectedBrand);
+    this.oneProduct.brand=this.selectedBrand;
     this.productsService.update(this.oneProduct)
       .subscribe(
         response => {

e1507d590670f1c0de5e9dad0b7e851ee96fc947
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-09-26 11:32:23 +0200

    beautify

--- webshop-express/public/javascripts/addToBasket.js
+++ webshop-express/public/javascripts/addToBasket.js
@@ -1,53 +1,25 @@
-// document.getElementsByClassName('formClass').addEventListener("click", function (event) {
-//   event.preventDefault();
-// })
-
 const addToBasket = function (productId, userId) {
-  // event.preventDefault();
 
+  // Value read from document
   const quantity = parseInt(document.querySelector('.prodNumber').value);
 
-  let xhr = new XMLHttpRequest();
-  const url = 'http://localhost:3000/basket';
-
-
-  xhr.open('post', url, true);
-
-  xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
-
-  xhr.onreadystatechange = function () {
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      // Request finished. Do processing here.
-      console.log(xhr.responseText);
-    }
-  }
-
-  const dataArray = {
+  // Data creating
+  var url = 'http://localhost:3000/basket';
+  var data = {
     user: userId,
     snowboardId: productId,
     quantity: quantity,
-  }
-  const data = JSON.stringify(dataArray);
+  };
 
-
-  console.log(data);
-  xhr.send(data);
-
-
-  // var url = 'http://localhost:3000/basket';
-  // var data = {
-  //   snowboardId: productId,
-  //   quantity: quantity,
-  // };
-
-  // fetch(url, {
-  //     method: 'POST', // or 'PUT'
-  //     body: JSON.stringify(data), // data can be `string` or {object}!
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }).then(res => res.json())
-  //   .catch(error => console.error('Error:', error))
-  //   .then(response => console.log('Success:', response));
+  // Fetch http req
+  fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
 
 }

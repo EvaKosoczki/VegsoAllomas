29cb234faa2e4f6c72784eb6cd8b8d109e38d473
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2019-09-30 11:43:52 +0200

    12-story: dashboard data

--- webshop-angular/src/app/pages/dashboard/dashboard.component.ts
+++ webshop-angular/src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { Product } from 'src/app/model/product';
 import { ProductsService } from 'src/app/service/products.service';
 import { User } from 'src/app/model/user';
 import { UserService } from 'src/app/service/user.service';
+import { Order } from 'src/app/model/order';
+import { OrderService } from 'src/app/service/orders.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,17 +14,23 @@ import { UserService } from 'src/app/service/user.service';
 export class DashboardComponent implements OnInit {
   allUser: User[] = [];
   allProduct: Product[] = [];
+  allOrder: Order[] = [];
 
   constructor(
     private productService: ProductsService,
-    private userService: UserService) {
+    private userService: UserService,
+    private orderService: OrderService) {
     this.productService.getAll().subscribe(products => this.allProduct = products);
     this.userService.getAll().subscribe(users => this.allUser = users);
+    this.orderService.getAll().subscribe(orders => {
+      this.allOrder = orders[0];
+    })
   }
 
   ngOnInit() {
   }
 
+  //Users:
   numberOfCustomers() {
     return this.allUser.filter(item => item.role == 'customer').length;
   }
@@ -31,10 +39,14 @@ export class DashboardComponent implements OnInit {
     return this.allUser.filter(item => item.role == 'administrator').length;
   }
 
+  //Products:
   numberOfProducts() {
     return this.allProduct.length;
   }
 
-
+  //Orders:
+  numberOfOrders() {
+    return this.allOrder.length;
+  }
 
 }

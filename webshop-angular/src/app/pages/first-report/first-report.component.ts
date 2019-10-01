7f4aa5eb9c635b4ffb4d6548fa5cbd9dcdc8e68d
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2019-10-01 09:36:51 +0200

    13-story: 2 buttons for reports

--- webshop-angular/src/app/pages/first-report/first-report.component.ts
+++ webshop-angular/src/app/pages/first-report/first-report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from 'src/app/service/orders.service';
 
 @Component({
   selector: 'app-first-report',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-report.component.css']
 })
 export class FirstReportComponent implements OnInit {
+  allOrder: any[] = [];
+  changeCounter: number = 0;
+  productsQuantityArr: [] = [];
+  totalPriceArr: [] = [];
+  orderDetails: any[] = [];
 
-  constructor() { }
+  constructor(private orderService: OrderService) {
+    this.orderService.getAll().subscribe(orders => {
+      this.allOrder = orders[0];
+      this.orderDetails = orders[1];
+      this.totalPriceArr = orders[2];
+      this.productsQuantityArr = orders[3];
+      console.log('Orders: ', orders);
+    });
+  }
 
   ngOnInit() {
   }

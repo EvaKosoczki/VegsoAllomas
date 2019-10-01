47a05e64a34dcc7bc49426db91d08cb8ec6863f7
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-10-01 22:29:46 +0200

    orderTotals function

--- webshop-express/routes/orders.js
+++ webshop-express/routes/orders.js
@@ -7,62 +7,66 @@ isHidden = true;
 
 //get all basket item and details
 router.get('/', async (req, res, next) => {
-  const ordersByCust = await db.get({
-    select: {
-      'OrderId': 'OrderId'
-    },
-    from: 'orders',
-    join: {
-      join: 'inner',
-      table: 'users',
-      'orders.userId': 'users.userId'
-    },
-  });
-  console.log(ordersByCust)
-  let Orders = []
-  for (let i = 0; i < ordersByCust.length; i += 1) {
-    let orderDetails = await db.get({
-      select: '*',
-      from: 'orders',
-      where: {
-        userId: `${req.user.userId}`,
-        relation: 'and',
-        OrderId: ordersByCust[i].OrderId
-      },
-      join: {
-        join: 'inner',
-        table: '`order-details`',
-        'orders.orderId': '`order-details`.order',
-        join1: 'inner',
-        table1: '`snowboards`',
-        'snowboards.ID': '`order-details`.snowboardId'
-      },
-    });
-    Orders.push(orderDetails);
-  }
-  console.log(Orders)
+	const ordersByCust = await db.get({
+		select: {
+			'OrderId': 'OrderId'
+		},
+		from: 'orders',
+		join: {
+			join: 'inner',
+			table: 'users',
+			'orders.userId': 'users.userId'
+		},
+	});
+	console.log(ordersByCust)
+	let Orders = []
+	for (let i = 0; i < ordersByCust.length; i += 1) {
+		let orderDetails = await db.get({
+			select: '*',
+			from: 'orders',
+			where: {
+				userId: `${req.user.userId}`,
+				relation: 'and',
+				OrderId: ordersByCust[i].OrderId
+			},
+			join: {
+				join: 'inner',
+				table: '`order-details`',
+				'orders.orderId': '`order-details`.order',
+				join1: 'inner',
+				table1: '`snowboards`',
+				'snowboards.ID': '`order-details`.snowboardId'
+			},
+		});
+		Orders.push(orderDetails);
+	}
 
 
+	let orderTotals = [];
+	let orderPrice = 0;
 
 
-  function totalPriceCounter(array) {
-    let orderTotalPrice = 0;
-    orderTotalPrice = array.map(item => item.price * item.quantity)
-      .reduce((a, b) => {
-        return a + b
-      }, 0)
-    return orderTotalPrice;
-  }
+	function totalPriceCounter(array) {
+		array.map(orders => {
+			orderPrice = orders.map(item =>
+					item.price * item.quantity).reduce((a, b) => {
+					return a + b
+				}, 0),
+				orderTotals.push(orderPrice)
+		})
+		console.log(orderTotals)
+		return orderTotals
+	}
 
-  res.render('orders', {
-    title: 'My orders',
-    basket: 'Basket Summary',
-    orderTotalPrice: totalPriceCounter(Orders),
-    Orders: Orders,
-    user: req.user,
-    counter: req.body.counter,
-    ordersByCust: ordersByCust,
-  });
+	res.render('orders', {
+		title: 'My orders',
+		basket: 'Basket Summary',
+		orderTotals: totalPriceCounter(Orders),
+		Orders: Orders,
+		user: req.user,
+		counter: req.body.counter,
+		ordersByCust: ordersByCust,
+	});
 
 });
 module.exports = router;

6d8e61c8fcd298195f930c848172428361e7b6a8
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2019-10-03 13:34:35 +0200

    Fix: date pipe

--- webshop-angular/src/app/pipe/custom-date.pipe.ts
+++ webshop-angular/src/app/pipe/custom-date.pipe.ts
@@ -9,7 +9,7 @@ export class CustomDatePipe implements PipeTransform {
     let yearMonth = JSON.stringify(value)
       .replace(/"[^"]*"\:/g, '')
       .replace(/[",\{\}]/g, '')
-      .replace(/\-..T..:00:00.000Z/, '');
+      .replace(/\-..T..:..:...000Z/, '');
     return yearMonth;
   }
 
